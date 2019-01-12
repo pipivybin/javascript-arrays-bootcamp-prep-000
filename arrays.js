301c6ae5b6bc9cd62dec727b1469e0a50456074e
Mingfang Duan <11203duan@gmail.com>
2019-01-12 20:56:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,6 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
+  return array
 }
