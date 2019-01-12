5aefcdf3f694de0b8e2889cb5720af4122af3b2c
Mingfang Duan <11203duan@gmail.com>
2019-01-12 20:41:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
