fdcf3465dd20922097f2395eac345075014a5400
Mingfang Duan <11203duan@gmail.com>
2019-01-12 19:42:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-function addElementToBeginningOfArray(array,element) {
+function addElementToBeginningOfArray(element) {
   return array.unshift(`element`)
 }
 
