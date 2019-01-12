b0a3d9b3282f7046c544633168e9f613a008a5ad
Mingfang Duan <11203duan@gmail.com>
2019-01-12 20:33:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
  array.unshift(element)
  return array
 }
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [array,element]
+}
